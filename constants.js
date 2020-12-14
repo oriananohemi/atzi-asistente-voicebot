3f7d128d3a68e524dd68bda9a6fea27f04e29401
Oriana <oriana.nohemi22@gmail.com>
2020-12-14 18:00:16 -0500

    cambio de flujo conversacional

---
+++ constants.js
@@ -0,0 +1,21 @@
+export const cities = ["Bogotá", "Ciudad de México", "bogota"];
+
+export const tematics = [
+  "El lago de los cisnes",
+  "Romeo y Julieta",
+  "El fantasma de la Ópera",
+];
+
+export const online = {
+  day: "22 de enero de 2021",
+  time: "7 pm",
+  topic: "Sueño de una noche de verano",
+};
+
+export const suggestion = ["Obras de teatro", "Festivales", "Eventos en Línea"];
+
+export const info = {
+  day: "15 de enero de 2021",
+  place: "Centro Cultura Ejemplo",
+  time: "8 pm",
+};
--- index.js
+++ index.js
@@ -1,72 +1,52 @@
 const functions = require("firebase-functions");
 const { WebhookClient } = require("dialogflow-fulfillment");
 const { Card, Suggestion } = require("dialogflow-fulfillment");
+const { info, cities, tematics, suggestion, online } = require("./constants");
 
 process.env.DEBUG = "dialogflow:debug"; // enables lib debugging statements
 
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest(
   (request, response) => {
-    const cities = ["Bogotá", "Ciudad de México", "bogota"];
-    const tematicas = [
-      "Dibujo artistico",
-      "Dibujo de caricaturas",
-      "Dibujo para niños",
-    ];
-    const events = {
-      day: "22 de noviembre de 2021",
-      time: "7 pm",
-      topic: "Taller de pintura",
-    };
-
-    const sugerencia = ["Platicas", "Talleres", "Live"];
-
     const agent = new WebhookClient({ request, response });
-    console.log(
-      "Dialogflow Request headers: " + JSON.stringify(request.headers)
-    );
-    console.log("Dialogflow Request body: " + JSON.stringify(request.body));
 
     function getCity(agent) {
-      if (cities.includes(agent.parameters.ciudad)) {
-        agent.add(`Genial, te puedo ayudar a encontrar
-      		talleres en tu ciudad o eventos en linea.
-            Por cual te gustaria empezar?
-            `);
-        sugerencia.forEach((tematica) => agent.add(new Suggestion(tematica)));
+      const agentParameters = agent.parameters.ciudad;
+      const
+      if (cities.includes(agentParameters)) {
+        agent.add(`Me encanta ${agentParameters}, te puedo ayudar a encontrar
+        obras de teatro, festivales o eventos en línea.
+        ¿A cuál deseas asistir?`);
+        suggestion.forEach((tematica) => agent.add(new Suggestion(tematica)));
       } else {
-        agent.add(`El proximo evento en linea es ${events.day} a 
-    	las ${events.time} y el tema es ${events.topic} `);
+        agent.add(`En los proximos días no hay registros de obras de teatro o festivales pero el próximo evento en línea es ${online.day} a
+    	  las ${online.time} y el tema es ${online.topic} `);
       }
     }
 
     function tematic(agent) {
-      const ssml = `<speak>Super!. A mi también me encantan los retos,
-      estos son los temas que se van a crubir próximamente en tu ciudad:
-      ¿Cual te interesa mas?</speak>`;
+      const ssml = `<speak>¡Genial!.Estos son los titulos que estaran disponibles los próximos días en tu ciudad:
+      ¿Cual te interesa?</speak>`;
       let conv = agent.conv();
       conv.ask(ssml);
       agent.add(conv);
       agent.add(
-        tematicas.forEach((tematica) =>
-          agent.add(new Suggestion(tematica).join('<break time="300ms">'))
+        tematics.forEach((tematic) =>
+          agent.add(new Suggestion(tematic).join('<break time="300ms">'))
         )
       );
     }
 
     function tematicDeep(agent) {
-      agent.add(`Super!. A mi tambien me encantan los retos,
-    estos son los temas que se van a crubir proximamente en tu ciudad:
-    ${tematicas.join(",")}
-    ¿Cual te interesa mas?
-    `);
+      agent.add(`<speak>¡Genial!.Estos son los titulos que estaran disponibles los próximos días en tu ciudad:
+      ¿Cual te interesa?</speak>`);
       agent.add(
-        tematicas.forEach((tematica) => agent.add(new Suggestion(tematica)))
+        tematics.forEach((tematic) => agent.add(new Suggestion(tematic)))
       );
     }
 
     function detail(agent) {
-      agent.add(`El proximo evento en linea es ${events.day} a 
-    	las ${events.time} y el tema es ${events.topic} `);
+      agent.add(`Esta función se realizará en el ${info.place} el ${info.day} a
+    	las ${info.time}. ¿Deseas agendar? `);
     }
 
     function detailEvent(agent) {
@@ -75,17 +55,17 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest(
     }
 
     function register(agent) {
-      agent.add(`<speak>Ok!, Solo nos falta un detalle tecnico. 
-     	Para asistir debes registrarte en <sub alias ="la plataforma de Meetup">https://www.meetup.com/</sub>
+      agent.add(`<speak>Ok!, Solo nos falta un detalle tecnico.
+     	  Confirma tus entradas ingresando a tu <sub alias ="gmail">https://www.gmail.com/</sub>
         ¿Te gustaria explorar algo mas?. Solo di: Quiero informacion
-        de otro taller, platica o evento</speak>
+        de otro teatro, festival o evento en línea</speak>
         `);
       agent.add(
         new Card({
-          title: "Registro en Meetup",
-          imageUrl: "https://www.meetup.com/",
-          text: "Registrate en Meetup",
-          buttonText: "Registrarme",
+          title: "Confirmar entradas",
+          imageUrl: "https://www.gmail.com/",
+          text: "Confirma entradas por gmail",
+          buttonText: "Confirmar",
           buttonUrl: "",
         })
       );
@@ -93,7 +73,7 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest(
     }
 
     let intentMap = new Map();
-    intentMap.set("Obtener Ciudad", getCity);
+    intentMap.set("City", getCity);
     intentMap.set("Live", detail);
     intentMap.set("Talleres", tematic);
     intentMap.set("Talleres - Deep Links", tematicDeep);
